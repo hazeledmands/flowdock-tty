1cc72b57053e74b3319663fe4e66887e7808e728
Max Edmands <me@maxedmands.com>
2012-10-20 08:07:38 -0700

    ChatInterface outputs to log on key presses, responsible for quitting on ctrl-c.

--- chat_interface.js
+++ chat_interface.js
@@ -1,3 +1,5 @@
+var keypress = require('keypress');
+var fs = require('fs');
 var panels = require('./panels');
 var logPanels = require('./log_panels');
 var commanderPanels = require('./commander_panels');
@@ -6,14 +8,27 @@ var ttyPanel = new panels.TTYPanel(process.stdout);
 var splitPanel = ttyPanel.split(5);
 var logPanel = new logPanels.LogPanel();
 var commanderPanel = new commanderPanels.CommanderPanel({ inputStream: process.stdin });
-var text = new logPanels.TextItemPanel({ text: "This is a test" });
-var text2 = new logPanels.TextItemPanel({ text: "This is a second test" });
+
+var log = fs.createWriteStream('debug.log', { flags: 'a' });
 
 splitPanel.topSplit.add(logPanel);
 splitPanel.bottomSplit.add(commanderPanel);
-logPanel.add(text);
-logPanel.add(text2);
 ttyPanel.render();
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
+
+keypress(process.stdin);
+process.stdin.on('keypress', function(chunk, key) {
+  var text = 'key: ' + JSON.stringify(key);
+  text += ' chunk: ' + chunk;
+  text += ' chunk length: ' + chunk.length;
+  text += "\n";
+
+  log.write(text);
+  logPanel.add(new logPanels.TextItemPanel({ text: text }));
+  if (key && key.ctrl && key.name == 'c') {
+    process.exit();
+  }
+});
+
